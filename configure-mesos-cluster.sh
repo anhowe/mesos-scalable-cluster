b1057088d32facd6e8057d039f5965fdfa53e411
anhowe <ahowe_ca@yahoo.ca>
2015-09-23 16:03:04 -0700

    fix the resolver

--- configure-mesos-cluster.sh
+++ configure-mesos-cluster.sh
@@ -284,6 +284,7 @@ if ismaster ; then
   sudo wget https://github.com/mesosphere/mesos-dns/releases/download/v0.2.0/mesos-dns-v0.2.0-linux-amd64.tgz
   sudo tar zxvf mesos-dns-v0.2.0-linux-amd64.tgz
   sudo mv mesos-dns-v0.2.0-linux-amd64 /usr/local/mesos-dns/mesos-dns
+  RESOLVER=`cat /etc/resolv.conf | grep nameserver | tail -n 1 | awk '{print $2}'`
 
   echo "
 {
@@ -292,6 +293,7 @@ if ismaster ; then
   \"ttl\": 0,
   \"domain\": \"mesos\",
   \"port\": 53,
+  \"resolvers\": [\"$RESOLVER\"],
   \"timeout\": 1,
   \"listener\": \"0.0.0.0\",
   \"email\": \"root.mesos-dns.mesos\",
